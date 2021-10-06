20ce04b250c0bedca26f6452af81268ca1eabc0e
AJ <aj@smartnvm.com>
2021-10-06 01:27:12 +0000

    implemented tweet character count

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(() => {
+
+  let keyPressed = true
+
+  $('#tweeter-text').on('keypress keydown keyup change', function (e) {
+    // let key
+    // key = String.fromCharCode(e.keyCode);
+    
+    let chars = 140 - $('#tweeter-text').val().length
+    
+    //update character count
+    $('#counter').text(chars);
+  
+
+    //update counter if backspace or delete keys are pressed
+    if ((e.keyCode == 8) || (e.keyCode == 46)) {
+      chars++
+    }
+
+    if (chars === 0) {
+      e.preventDefault();
+      $(this).parent("form").children("div").children("output").addClass("red")
+
+    } else {
+      $(this).parent("form").children("div").children("output").removeClass("red");
+    }
+
+  })
+
+});
